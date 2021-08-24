ea6d0609ac6607827e5d1195b5f353f061a5b80f
Oleg Gupalo <gupalooleg@gmail.com>
2021-08-24 22:54:59 +0300

    feat: implement 'check' function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsPairs = bracketsConfig.map((e) => e.join(""));
+    while (
+        bracketsPairs.some((e) => {
+            const i = str.indexOf(e);
+            if (i != -1) {
+                str = str.slice(0, i) + str.slice(i + e.length);
+                return true;
+            }
+            return false;
+        })
+    );
+    return !str;
+};
